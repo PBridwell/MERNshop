5407cafe8847ced2f762cd155cc4447a6b3978b2
unknown <pbrid32@gmail.com>
2020-11-06 13:39:06 -0600

    adds delete and post route

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // Item model
-const Item = require('../../models/Item');
+const Item = require('../../models/Item')
 
 // @route GET api/items
 // get ALL items
@@ -10,7 +10,27 @@ const Item = require('../../models/Item');
 router.get('/', (req, res) => {
 	Item.find()
 		.sort({ date: -1 })
-		.then((items) => res.json(items));
-});
+		.then(items => res.json(items))
+})
 
-module.exports = router;
+// @route POST api/items
+// create a new Item and post
+// @access public
+router.post('/', (req, res) => {
+	const newItem = new Item({
+		name: req.body.name,
+	})
+
+	newItem.save().then(item => res.json(item))
+})
+
+// @route DELETE api/items/:id
+// DELETE single Item
+// @access public
+router.delete('/:id', (req, res) => {
+	Item.findById(req.params.id)
+		.then(item => item.remove().then(() => res.json({ success: true })))
+		.catch(err => res.status(404).json({ success: false }))
+})
+
+module.exports = router
