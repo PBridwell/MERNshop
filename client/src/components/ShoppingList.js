565bc8311c850a8a3ad00ca45e82f01bd9540c4f
unknown <pbrid32@gmail.com>
2020-12-14 13:56:36 -0600

    updates shoppinglist component to add new item with functional button

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 
 function ShoppingList(props) {
 	const [items, setItems] = useState([
@@ -10,7 +10,24 @@ function ShoppingList(props) {
 		{ id: uuid(), name: 'Rice' },
 		{ id: uuid(), name: 'Steak' },
 	]);
+
+	return (
+		<Container>
+			<Button
+				color='dark'
+				style={{
+					marginBottom: '2rem',
+				}}
+				onClick={() => {
+					const name = prompt('Enter Item');
+					if (name) {
+						setItems((items) => [...items, { id: uuid(), name }]);
+					}
+				}}>
+				Add Item
+			</Button>
+		</Container>
+	);
 }
-return;
 
 export default ShoppingList;
